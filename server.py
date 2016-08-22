2a3198a62d241ea9c2a320dcaeec20dd348febde
Ubuntu <ubuntu@ip-172-31-22-165.us-west-2.compute.internal>
2016-08-22 23:48:57 +0000

    adding changes to deploy live

--- server.py
+++ server.py
@@ -32,4 +32,4 @@ def convert():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0')
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from flask-file-conversion import app
+
+if __name__=='__main__':
+    app.run()
